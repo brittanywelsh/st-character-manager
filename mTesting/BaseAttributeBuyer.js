78543fe42479a8234bfc6e9b98762553cbddfd23
Mike Pors <mike.pors@gmail.com>
2015-08-05 01:04:10 -0600

    -Recovered my BaseAttributeBuyer.js file for development purposes. -"Forked" a copy of the Character Controller for me to edit should I see fit as per our previous discussion. -

--- mTesting/BaseAttributeBuyer.js
+++ mTesting/BaseAttributeBuyer.js
@@ -1,16 +1,55 @@
+var lCharacters = [];
+
+//Two characters are made here for testing purposes.
+lCharacters.push(Game.CreateNewCharacter());
+lCharacters.push(Game.CreateNewCharacter());
+
+lCharacters[0].index = 0;
+lCharacters[1].index = 1;
 
 $(document).ready(function(){ 
-
+                   
+  //Pushing any button clears message box
+  $("button").on('click', ClearOldMessage);  
+  
+  //Adds a new character when the appropriate button is pushed.
+  $("#add-char").on('click', function(){
+    lCharacters.push(Game.CreateNewCharacter());
+    lCharacters[lCharacters.length - 1].index = lCharacters.length - 1;
+    AddToViewList(lCharacters[lCharacters.length - 1]);
+  }); 
+  
+  //Sets up the infrastructure for switching characters when the character name is pushed
+  $("#char-list").on('click', 'td', BindCharacterToView);
+   
+  //Sets selected character to gray
+  $('#Character0').css('background', 'lightgray');
+  
   //Attaches a click event listener to the + and - buttons that changes attribute values appropriately   
   Game.AttributeList.forEach(function(sAttributeName){
     $("#"+sAttributeName + "+").on('click', ChangeAttributePoints);
     $("#"+sAttributeName + "-").on('click', ChangeAttributePoints);
   });
   
+  //Adds existing characters to the list of viewable characters                                                                                            
+  lCharacters.forEach(AddToViewList); 
+  
   //Makes the screen display data for the first character
   BindCharacterToView(lCharacters[0]);
   
 });
+
+//Adds a character to the list of viewable characters
+function AddToViewList(Character){
+  var CharViewList = document.getElementById("char-list");
+  var newRow = CharViewList.insertRow(Character.index);
+  var newCell = newRow.insertCell(0);
+  newCell.innerHTML = Character.Name || "Character " + Character.index; 
+  newCell.className = "char";
+  newCell.id = "Character" + Character.index;   
+  $("#" + newCell.id).data("character-index", Character.index);
+
+}
 
 //Takes a given character and displays it.
 function BindCharacterToView(Character){
@@ -32,7 +71,8 @@ function BindCharacterToView(Character){
   $('.char').css('background', 'white');
   $('#' + id).css('background', 'lightgray');
 
-  Game.AttributeList.forEach(AssignAttribute, Character);    
+  Game.AttributeList.forEach(AssignAttribute, Character);
+    
   
   $("#PTS").data("character-index", Character.index);
   $("#PTS").text(Character.RemainingAttributeBuyPoints);
@@ -43,5 +83,44 @@ function BindCharacterToView(Character){
   $("#class-list-2").val(Character.Classes[1] || "select");
   $("#class-list-3").val(Character.Classes[2] || "select");
 };
-     
   
+
+function AssignAttribute(sAttributeName){
+
+  $("#"+sAttributeName).data("character-index", this.index);
+  $("#"+sAttributeName).text(this.AttributeScore(sAttributeName));
+  
+}            
+  
+
+function ChangeAttributePoints(event){
+
+  var buttonID = event.target.id;
+  var attribute = buttonID.substr(0, 3);// string.substr(start, length)
+  var modifier;
+  var Character = lCharacters[$("#"+attribute).data("character-index")];
+    
+  switch(buttonID.slice(-1)){
+    case '-':
+      modifier = -1;
+      break;
+    case '+':
+      modifier = 1;
+      break
+  }  
+  
+  try{
+    Game.BuyAttributePointsForChar(Character, attribute, modifier);
+  }
+  catch(e){
+    $('#message').text(e.message);  
+  }                              
+  finally{
+    $("#PTS").text(Character.RemainingAttributeBuyPoints);
+    $("#"+attribute).text(Character.AttributeScore(attribute));  
+  }
+}
+
+function ClearOldMessage(){
+  document.getElementById("message").innerHTML = "";
+}
---
+++ mTesting/CharacterControllerPorsFork.js
@@ -0,0 +1,126 @@
+var CharacterController = (function () {
+
+  /* Local Variables */
+  var maxNumberOfCharacters = 15;
+  var listOfCharacters = [];
+  var indexOfSelectedCharacter = 0;
+
+  /* Methods */
+  var createNewCharacter = function(newCharacterName, sLevel, sGender, sAlignment) {
+    try {
+      // check whether character list is too big
+      if (listOfCharacters.length >= maxNumberOfCharacters)
+        throw "That's too many characters. You can only have " + maxNumberOfCharacters + " at a time. Try removing one of the active characters.";
+      // create new character object
+      var newCharacter = new Character(newCharacterName, sLevel, sGender, sAlignment);
+      // add appropriate index:
+      var newCharacterIndex = listOfCharacters.length;
+      newCharacter.index = newCharacterIndex;
+      // add to character list:
+      listOfCharacters.push(newCharacter);
+      indexOfSelectedCharacter = newCharacterIndex;
+      // append the appropriate view objects:
+      CharacterView.addToCharacterListView(newCharacter, newCharacterIndex);
+      // console stuff...
+      console.log("success! new listOfCharacters is:");
+      listOfCharacters.forEach( function(item) {console.log(item.Name, item.index)});
+    }
+    catch (error) {
+      alert(error);
+      return null;
+    }
+  };
+
+  var removeSelectedCharacterByIndex = function(CharacterIndex) {
+    console.log("clicked a thing");
+    try {
+      // check whether character list is too big
+      if (~ characterIndex) {
+        throw "ERROR: Cannot remove character without appropriate index.";
+      }
+      // create new character object
+      var newCharacter = new Character(newCharacterName, sLevel, sGender, sAlignment);
+      // add appropriate index:
+      var newCharacterIndex = listOfCharacters.length;
+      newCharacter.index = newCharacterIndex;
+      // add to character list:
+      listOfCharacters.push(newCharacter);
+      // append the appropriate view objects:
+      CharacterView.addToCharacterListView(newCharacter, newCharacterIndex);
+      // console stuff...
+      console.log("success! new listOfCharacters is:");
+      listOfCharacters.forEach( function(item) {console.log(item.Name, item.index)});
+    }
+    catch (error) {
+      alert(error);
+      return null;
+    }
+  };
+
+  var selectCharacterByIndex = function(CharacterIndex) {
+    indexOfSelectedCharacter = Number(CharacterIndex);
+    return listOfCharacters[indexOfSelectedCharacter];
+  }
+
+  var getSelectedCharacter = function() {
+    return listOfCharacters[indexOfSelectedCharacter];
+  }  
+  
+  
+
+  /* Public Methods */
+  return {
+    createNewCharacter: createNewCharacter,
+    getSelectedCharacter: getSelectedCharacter,
+    selectCharacterByIndex: selectCharacterByIndex,
+    removeSelectedCharacterByIndex: removeSelectedCharacterByIndex,
+  };
+
+})();
+
+/* jQuery Event Hooks */
+
+$(document).ready(function(){ 
+
+  // Adds a new character.
+  $("#button-new-character").on('click', function() {
+    CharacterController.createNewCharacter("Betsy", 1, "gal", "bad");
+  });
+
+  // Change selected character.
+  $("#character-list").on('click', 'li', function() {
+    var indexString = this.id.substring(19);
+    CharacterController.selectCharacterByIndex(indexString);
+    console.log("clicked on (value):" + indexString);
+    console.log("currently selected char:" + CharacterController.getSelectedCharacter(indexString).Name);
+  });
+
+  // Remove selected character from application memory.
+  $("#button-remove-character").on('click', function() {
+    CharacterController.removeSelectedCharacterByIndex();
+  });
+
+});
+
+
+//Takes a given character and displays it.
+/*function bindCharacterToView(Character){
+  var id;*/
+  /*
+   * This function is actually two rolled into one.  The first changes the viewed
+   * character to the one most recently clicked.  The second is the one which 
+   * initially handles binding a character (i.e. without any clicking having 
+   * occurred.
+   */
+/*  if (Character.target){
+    id = Character.target.id;
+    Character = listOfCharacters[$('#' + id).data("character-index")];
+  }
+  else{
+    id = "Character" + Character.index;
+  }
+  
+  $('.char').css('background', 'lightblue');
+  $('#' + id).css('background', '#000');
+};*/
+

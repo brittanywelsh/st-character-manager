77feadfc809f01eec44aceff44ae2df0f09b9c44
Mike Pors <mike.pors@gmail.com>
2015-08-05 01:19:26 -0600

    After taking a brief look into Test Driven Development tools I decided I wasn't incredibly keen on learning how to use what was available.  So I wrote a UnitTester of my own, and tested it.  The UnitTestTester provides a nice example of how the class works.  It is fully functional and useable now. --BASIC INSTRUCTIONS-- 1-Instantiate a unit test object "uTest" 2-Write a boolean valued function, f, to be your test.  An output of 'true' is considered a success. 3-call "uTest.addTest(f);" 4-repeat steps 2 and 3 as often as necessary. 5-call "uTest.runInHTML()", run in any HTML document.  The table giving the results is constructed by the mothod. 5alts - call "uTest.run()" or "uTest.runTest(sTestName)" to get an object (respectively string) containing the results of all your tests.

---
+++ mTesting/Testing Material/UnitTest.js
@@ -0,0 +1,72 @@
+function UnitTest(){
+  var Tests = new Object();  
+         
+  this.run = function(){ 
+    var TestResults = new Object();                                 
+    Object.keys(Tests).forEach(function (sTestName){ 
+      try{
+        var bOutput = Tests[sTestName]();
+        if (typeof bOutput != "boolean"){
+          TestResults[sTestName] = "Bad test! Did not generate boolean value!";
+        } 
+        else {
+          TestResults[sTestName] = (bOutput) ? "Passed!": "Failed!"; 
+        }
+      } 
+      catch(e){  
+        if ( e instanceof Error) TestResults[sTestName] = "Threw exception: " + e.message;
+        else TestResults[sTestName] = "Threw exception: " + e; 
+      }
+    });
+    return TestResults;
+  }
+  
+  this.runTest = function (sTestName){
+    var TestResult = "";                                 
+    try{
+      var bOutput = Tests[sTestName]();
+      if (typeof bOutput != "boolean"){
+        TestResult = "Bad test! Did not generate boolean value!";
+      } 
+      else {
+        TestResult = (bOutput) ? "Passed!": "Failed!"; 
+      }
+    } catch(e){
+        TestResult = "Threw exception: " + e.message; 
+    }
+  return TestResult;
+  }
+  
+  this.addTest = function(sTestName, fNewTest){
+    try{
+      if (Tests[sTestName]){
+        throw new Error("Test name already selected.  Try another name");
+      }
+    }
+    catch(e){
+      return;
+    }        
+    Tests[sTestName] = fNewTest;
+  } 
+  
+  this.removeTest = function(sTestName){
+    delete Tests[sTestName];
+  }
+  this.getTestNames = function(){
+    return Object.keys(Tests);
+  }
+}
+
+UnitTest.prototype.runInHTML = function(){
+  var TestTable = document.createElement('Table');
+  TestTable.border = "1"; 
+  document.body.appendChild(TestTable);
+  var Results = this.run();
+  this.getTestNames().forEach(function (sTestName){
+    var newRow = TestTable.insertRow();
+    var nameCell = newRow.insertCell();
+    nameCell.innerHTML = sTestName + " result:"
+    var displayCell = newRow.insertCell();
+    displayCell.innerHTML = Results[sTestName]; 
+  } );
+}

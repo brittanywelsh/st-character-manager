4623d6952e7ff9a9a9086167de5031996f6239e7
Mike Pors <mike.pors@gmail.com>
2015-09-11 18:02:26 -0600

    Missed this file in the last commit.  Whoops!

---
+++ js/GameData.js
@@ -0,0 +1,13 @@
+/*global Attributes */
+
+Attributes.Data = {
+    FeatureNames:
+        [
+            'Strength',
+            'Dexterity',
+            'Intelligence',
+            'Wisdom',
+            'Constitution',
+            'Charisma'
+        ]
+};
